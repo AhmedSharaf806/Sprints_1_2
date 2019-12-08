66a43ffffd62d114739fb71e5653373735c5c47c
Ahmed Sharaf <ahmedsharaf806@gmail.com>
2019-12-08 18:02:12 +0200

    Update timers.c

--- timers.c
+++ timers.c
@@ -9,6 +9,7 @@
 #include "timers.h"
 
 static uint8 gu8_timer0Prescaler;
+static uint8 gu8_timer1Prescaler;
 
 
 /*===========================Timer0 Control===============================*/
@@ -80,33 +81,27 @@ void timer0Delay_ms(uint16 delay){
 
 	    case 0: //Normal Mode
 
-		TCCR0 = T0_NORMAL_MODE | T0_NO_CLOCK | T0_PRESCALER_64;
+		TCCR0 |= T0_PRESCALER_64;
 
 		uint16 u16_OverflowCounterNM=0; //Count number of overflows
-
-		uint8  u8_flagCheckNM; //Check overflow
 
 		timer0Set(130); //130 to trick only 125 tricks for 1 overflow
 
 		timer0Start();
 
-		while(u16_OverflowCounterNM != (0.5*delay)) //check delay equal overflow
-		    {
-			u8_flagCheckNM=0;
-			u8_flagCheckNM=GET_BIT(TIFR,BIT0); //store status of TOV0 flag
 
-			if(u8_flagCheckNM==1)//check for overflow
-			    {
-				u16_OverflowCounterNM++; //increase overflow by 1 everytime
-				SET_BIT(TIFR,BIT0);
-			    }
+		for(u16_OverflowCounterNM=0; u16_OverflowCounterNM<delay; u16_OverflowCounterNM++)
+		    {
+			while(((TIFR)&(1<<0))==0){};
+			SET_BIT(TIFR,BIT0);
+			timer0Set(130);
 		    }
 		timer0Stop();
 		break;
 
 	    case 1: //Compare Mode
 
-		TCCR0 = T0_COMP_MODE | T0_NO_CLOCK | T0_PRESCALER_64;
+		TCCR0 |= T0_PRESCALER_64;
 
 		uint16 u16_OverflowCounterCM=0; //Count number of overflows
 
@@ -140,3 +135,106 @@ void timer0SwPWM(uint8 dutyCycle,uint8 freq){
 
 
 }
+
+
+/*===========================Timer1 Control===============================*/
+/**
+ * Description:
+ * @param controlA
+ * @param controlB
+ * @param initialValue
+ * @param outputCompare
+ * @param interruptMask
+ */
+void timer1Init(En_timer1Mode_t mode,En_timer1OC_t OC,En_timer1perscaler_t prescal, uint16 initialValue, uint16 outputCompareA, uint16 outputCompareB, uint16 inputCapture, En_timer1Interrupt_t interruptMask){
+
+    TCCR1 = mode | OC;
+
+    gu8_timer1Prescaler=prescal;
+
+    TCNT1 = initialValue;
+
+    OCR1A = outputCompareA;
+
+    OCR1B = outputCompareB;
+
+    ICR1 = inputCapture;
+
+    TIMSK = interruptMask;
+
+
+}
+
+/**
+ * Description:
+ * @param value
+ */
+void timer1Set(uint16 value){
+
+    TCNT1 = value;
+}
+
+
+/**
+ * Description:
+ * @return
+ */
+uint16 timer1Read(void){
+
+    return TCNT1;
+}
+
+
+
+/**
+ * Description:
+ */
+void timer1Start(void){
+
+    TCCR1 |=gu8_timer1Prescaler;
+}
+
+/**
+ * Description:
+ */
+void timer1Stop(void){
+
+    TCCR1 = T1_NO_CLOCK;
+
+}
+
+/**
+ * Description:
+ * @param delay
+ */
+void timer1Delay_ms(uint16 delay){
+
+		timer1Init(T1_NORMAL_MODE,T1_OC1_DIS,T1_PRESCALER_64,0,0,0,0,T1_POLLING);
+
+
+    		uint32 u32_OverflowCounterNM=0; //Count number of overflows
+
+    		timer1Set(65410); //65410 to trick only 125 tricks for 1 overflow
+
+    		timer1Start();
+
+		for(u32_OverflowCounterNM=0; u32_OverflowCounterNM<delay; u32_OverflowCounterNM++)
+		    {
+			while(((TIFR)&(1<<2))==0){};
+			SET_BIT(TIFR,BIT2);
+			timer1Set(65410);
+		    }
+    		timer1Stop();
+
+}
+
+/**
+ * Description:
+ * @param dutyCycle
+ */
+void timer1SwPWM(uint8 dutyCycle,uint8 freq){
+
+
+    
+}
+
